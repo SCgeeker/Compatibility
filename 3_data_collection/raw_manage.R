4352a7c29c3d8b945ae9407079c651642b599037
Sau-Chin Chen <pmsp96@gmail.com>
2015-12-30 12:09:06 +0800

    Finished the materials for 2_plan

--- 3_data_collection/raw_manage.R
+++ 3_data_collection/raw_manage.R
@@ -1,7 +1,9 @@
 if(!require(stringi)){install.packages("stringi")}
 library(stringi) # Procoss the variable names
 
-filenames <- c("quickrun-A.csv","quickrun-B.csv")
+setwd("../3_data_collection") # Run this line if you do not continue the work after downloaded files
+
+filenames <- c("A-0.csv","test-B.csv")
 
 # 定義原始資料檔名,隨下載後的檔案名稱調整
 # Init_name就是subject ID 
@@ -11,40 +13,32 @@ end_name <- ".csv"
 DF <- data.frame()  # 空白data frame，存放Stroop原始資料
 
 for(i in 1:length(filenames)  ){
-    subj0 <- read.csv(paste0("RAW/", filenames[i]), fileEncoding = "UTF-8")
-    subj0 <- subj0[subj0$practice == "no",c("response_time", "correct", "EXPA", "EXPB", "soa","Compatibility")] 
+    subj0 <- read.csv(paste0("RAW/", filenames[i]))
+    subj0 <- subj0[subj0$practice == "no" ,c("response_time", "correct", "EXPA", "EXPB", "soa","Compatibility")]  # Opensesame script has to have the accurate setting for cycles.
     
     subj_id <- gsub(end_name,"",filenames[i]) 
 
     print(subj_id)
     
-    subj0 <- data.frame(ID = rep(subj_id, dim(subj0)[1]), subj0)
+    subj0 <- data.frame(ID = rep(subj_id, dim(subj0)[1]), trial = seq(dim(subj0)[1]), subj0)
     DF <- rbind(DF, subj0)
     rm(subj0,subj_id)
 } 
 
+codebook_Raw <- data.frame(
+    Variable = names(DF),
+    Values = c("char","integer","numeric", "1,0", "yes,no", "yes,no","integer","y,n"),
+    Description = c(
+        "Filename is the subject ID",
+        "sequence of trial",
+        "raw response time at this trial",
+        "1: this is a correct response; 0: this is a incorrect response",
+        "yes: This is from EXPA, no: This is from EXPB",
+        "yes: This is from EXPB, no: This is from EXPA",
+        "soa of this trial",
+        "y: this is a compatible target; n: this is an incompatible target"
+    )
+)
+
 write.csv(DF, file = "Compatibility_Raw.csv", quote = FALSE, row.names = FALSE, fileEncoding = "UTF-8")
-
-EXPA_RAW <- DF[DF$EXPA == "yes" & DF$correct == 1,]
-EXPB_RAW <- DF[DF$EXPB == "yes" & DF$correct == 1,]
-
-EXPA_RT <- with(data = EXPA_RAW, tapply(response_time, paste("ID",paste("soa","Compatibility","_"),"_"),mean))
-EXPA_ACC <- with(data = EXPA_RAW, tapply(correct, paste("ID",paste("soa","Compatibility","_"),"_"),sum))/16
-
-EXPA <- data.frame(
-    matrix( unlist( stri_split_coll(names(EXPA_RT), "_") ), ncol = 5, byrow = TRUE, dimnames = list(NULL,c("ID","SOA","Compatibility"))),
-    RT = EXPA_RT,
-    ACC = EXPA_ACC,
-    row.names = NULL
-)
-
-
-EXPB_RT <- with(data = EXPB_RAW, tapply(response_time, paste("ID",paste("soa","Compatibility","_"),"_"),mean))
-EXPB_ACC <- with(data = EXPB_RAW, tapply(correct, paste("ID",paste("soa","Compatibility","_"),"_"),sum))/16
-
-EXPB <- data.frame(
-    matrix( unlist( stri_split_coll(names(EXPB_RT), "_") ), ncol = 5, byrow = TRUE, dimnames = list(NULL,c("ID","SOA","Compatibility"))),
-    RT = EXPB_RT,
-    ACC = EXPB_ACC,
-    row.names = NULL
-)
+write.csv(codebook_Raw, file = "Codebook_Raw.csv", quote = FALSE, row.names = FALSE, fileEncoding = "UTF-8")
